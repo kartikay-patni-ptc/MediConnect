aebdf6b95ee575d2dbc79ddacf7c9fb421b0c485
mahesh-marathe-ptc <mamarathe@ptc.com>
2025-08-11 13:43:54 +0530

    Update login.component.ts

--- MediConnect/mediconnect-frontend/src/app/auth/login/login.component.ts
+++ MediConnect/mediconnect-frontend/src/app/auth/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
-      password: Validators.required
+      password: ['',[Validators.required]]
     });
   }
 
